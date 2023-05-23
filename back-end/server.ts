da443baf01e740d6a0b62b7df0947cd61d2b4b82
Denis <deniskatarasenko6@gmail.com>
2023-05-23 15:14:14 +0300

    added mongoose connection

--- back-end/server.ts
+++ back-end/server.ts
@@ -1,12 +1,24 @@
 import http from "http";
 import { app } from "./app";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const server = http.createServer(app);
 dotenv.config();
 
 const port = process.env.SERVER_PORT || 4001;
+const ATLAS_URI = process.env.ATLAS_URI || "";
 
-server.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+const startServer = async () => {
+  try {
+    await mongoose.connect(ATLAS_URI);
+    server.listen(port, () => {
+      console.log(`MongoDB connected, server is on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB: ", error);
+    process.exit(1);
+  }
+};
+
+startServer();
